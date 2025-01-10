eb49d4f14db4108a1d5389752a6935fa1d89b9f3
samuelmsilva2v <samuelmsilva@outlook.com.br>
2025-01-10 17:07:31 -0300

    convidado controller

---
+++ src/main/java/com/example/demo/application/controllers/ConvidadoController.java
@@ -0,0 +1,51 @@
+package com.example.demo.application.controllers;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.domain.models.dtos.ConvidadoRequestDto;
+import com.example.demo.domain.models.dtos.ConvidadoResponseDto;
+import com.example.demo.domain.services.interfaces.ConvidadoDomainService;
+
+@RestController
+@RequestMapping("/api/convidados")
+public class ConvidadoController {
+
+	@Autowired
+	private ConvidadoDomainService convidadoDomainService;
+	
+	@PostMapping
+	public ConvidadoResponseDto insert(@RequestBody ConvidadoRequestDto request) {
+		return convidadoDomainService.cadastrarConvidado(request);
+	}
+
+	@PutMapping("{id}")
+	public ConvidadoResponseDto update(@PathVariable UUID id, @RequestBody ConvidadoRequestDto request) {
+		return convidadoDomainService.alterarConvidado(id, request);
+	}
+
+	@DeleteMapping("{id}")
+	public String delete(@PathVariable UUID id) {
+		return convidadoDomainService.excluirConvidado(id);
+	}
+
+	@GetMapping("{id}")
+	public ConvidadoResponseDto getById(@PathVariable UUID id) {
+		return convidadoDomainService.consultarConvidadoPorId(id);
+	}
+
+	@GetMapping
+	public List<ConvidadoResponseDto> getAll() {
+		return convidadoDomainService.consultarConvidados();
+	}
+}
