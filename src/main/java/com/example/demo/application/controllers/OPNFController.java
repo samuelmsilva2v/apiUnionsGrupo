302a4a24c71c36db0484576faedc7f9e266ad1a8
samuelmsilva2v <samuelmsilva@outlook.com.br>
2025-01-20 17:21:06 -0300

    opnf controller

---
+++ src/main/java/com/example/demo/application/controllers/OPNFController.java
@@ -0,0 +1,50 @@
+package com.example.demo.application.controllers;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.domain.models.dtos.OPNFRequestDto;
+import com.example.demo.domain.models.dtos.OPNFResponseDto;
+import com.example.demo.domain.services.interfaces.OPNFDomainService;
+
+@RestController
+@RequestMapping("/api/opnf")
+public class OPNFController {
+
+	@Autowired
+	private OPNFDomainService opnfDomainService;
+	
+	@GetMapping
+	public OPNFResponseDto post(@RequestBody OPNFRequestDto request) {
+		return opnfDomainService.criarOPNF(request);
+	}
+	
+	@PutMapping("{id}")
+	public OPNFResponseDto put(@PathVariable UUID id, @RequestBody OPNFRequestDto request) {
+		return opnfDomainService.alterarOPNF(id, request);
+	}
+	
+	@DeleteMapping("{id}")
+	public String delete(@PathVariable UUID id) {
+		return opnfDomainService.excluirOPNF(id);
+	}
+	
+	@GetMapping("{id}")
+	public OPNFResponseDto getById(@PathVariable UUID id) {
+		return opnfDomainService.consultarOPNFPorId(id);
+	}
+	
+	@GetMapping
+	public List<OPNFResponseDto> getAll() {
+		return opnfDomainService.consultarOPNFs();
+	}
+}
